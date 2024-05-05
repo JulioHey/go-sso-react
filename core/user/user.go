d9d58afd2e0e10aceede50cba0ee2d97b1471a20
JulioHey <66440692+JulioHey@users.noreply.github.com>
2024-05-05 17:30:47 -0300

    [FEATURE] Criação do modelo para salvar senha de usuário (#6)
    * Create UserPassword model and added it functions
    
    * Changed package version
    
    * Changed name of struct
    
    ---------
    
    Co-authored-by: Julio Hey <julio.hey@vocedm.com.br>

--- core/user/user.go
+++ core/user/user.go
@@ -3,10 +3,13 @@ package user
 import (
 	"authorizer/core/repository"
 	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var validate = validator.New()
 
+// User model
 type User struct {
 	repository.Model
 	Username  string `gorm:"unique;not null" json:"username" validate:"required"`
@@ -15,6 +18,54 @@ type User struct {
 	LastName  string `json:"last_name" validate:"required"`
 }
 
+// Validate user fields
 func (u *User) Validate() error {
 	return validate.Struct(u)
+}
+
+// CreateUserRequest model
+type CreateUserRequest struct {
+	User     User   `json:"user" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+func (c *CreateUserRequest) Validate() error {
+	return validate.Struct(c)
+}
+
+// Password model saves user encrypted password
+type Password struct {
+	repository.Model
+	Password string    `json:"password" validate:"required"`
+	UserID   uuid.UUID `json:"user_id" gorm:"index:userID;not null,column:user_id" validate:"required"`
+	User     User      `json:"-" validate:"-"`
+}
+
+// Validate user password fields
+func (u *Password) Validate() error {
+	return validate.Struct(u)
+}
+
+// HashPassword hashes user password and returns userPassword Model
+func HashPassword(password, userID string) (*Password, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := uuid.Parse(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Password{
+		Password: string(bytes),
+		UserID:   id,
+	}, nil
+}
+
+// CheckPasswordHash checks if password matches hash
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
--- core/user/user_test.go
+++ core/user/user_test.go
@@ -1,39 +1,82 @@
 package user
 
 import (
+	"errors"
+	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("User", func() {
-	Context("Validate", func() {
+	Context("User", func() {
+		Context("Validate", func() {
 
-		It("Valid User", func() {
-			user := &User{
-				Username:  "test",
-				Email:     "test@gmail.com",
-				FirstName: "test",
-				LastName:  "test",
+			It("Valid User", func() {
+				user := &User{
+					Username:  "test",
+					Email:     "test@gmail.com",
+					FirstName: "test",
+					LastName:  "test",
+				}
+				err := user.Validate()
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("Invalid Email", func() {
+				user := &User{
+					Username:  "test",
+					Email:     "test",
+					FirstName: "test",
+					LastName:  "test",
+				}
+				err := user.Validate()
+				Expect(err).To(HaveOccurred())
+			})
+
+			It("Invalid User", func() {
+				user := &User{}
+				err := user.Validate()
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+
+	Context("UserPassword", func() {
+		userID := uuid.New()
+		DescribeTable("Validate", func(input *Password, expectedErr error) {
+			err := input.Validate()
+			if expectedErr == nil {
+				Expect(err).ToNot(HaveOccurred())
+			} else {
+				Expect(err).To(HaveOccurred())
 			}
-			err := user.Validate()
-			Expect(err).ToNot(HaveOccurred())
-		})
+		},
+			Entry("Valid UserPassword", &Password{
+				Password: "123",
+				UserID:   uuid.New(),
+			}, nil),
+			Entry("Invalid UserPassword", &Password{
+				Password: "123",
+			}, errors.New("Teste")),
+		)
 
-		It("Invalid Email", func() {
-			user := &User{
-				Username:  "test",
-				Email:     "test",
-				FirstName: "test",
-				LastName:  "test",
-			}
-			err := user.Validate()
-			Expect(err).To(HaveOccurred())
-		})
-
-		It("Invalid User", func() {
-			user := &User{}
-			err := user.Validate()
-			Expect(err).To(HaveOccurred())
-		})
+		DescribeTable("HashPassword and CheckPassword",
+			func(password, userID string, expectedResult *Password) {
+				result, err := HashPassword(password, userID)
+				if expectedResult == nil {
+					Expect(err).To(HaveOccurred())
+					Expect(result).To(BeNil())
+				} else {
+					Expect(err).ToNot(HaveOccurred())
+					Expect(CheckPasswordHash(password, result.Password)).To(Equal(true))
+					Expect(CheckPasswordHash("password", result.Password)).To(Equal(false))
+					Expect(result.UserID).To(Equal(expectedResult.UserID))
+				}
+			},
+			Entry("Valid Password", "123", userID.String(), &Password{
+				UserID: userID,
+			}),
+			Entry("Valid Password", "123", "123", nil),
+		)
 	})
 })
