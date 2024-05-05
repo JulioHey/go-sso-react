0454f1a6e9c925d6ca23b8f7d7cba6a0f51dbc43
Julio Hey <julio.hey@vocedm.com.br>
2024-05-05 17:24:40 -0300

    Create UserPassword model and added it functions

--- core/user/user.go
+++ core/user/user.go
@@ -3,10 +3,13 @@ package user
 import (
 	"authorizer/core/repository"
 	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var validate = validator.New()
 
+// User model
 type User struct {
 	repository.Model
 	Username  string `gorm:"unique;not null" json:"username" validate:"required"`
@@ -15,6 +18,53 @@ type User struct {
 	LastName  string `json:"last_name" validate:"required"`
 }
 
+// Validate user fields
 func (u *User) Validate() error {
 	return validate.Struct(u)
+}
+
+// CreateUserRequest model
+type CreateUserRequest struct {
+	User     User   `json:"user" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+func (c *CreateUserRequest) Validate() error {
+	return validate.Struct(c)
+}
+
+// UserPassword model saves user encrypted password
+type UserPassword struct {
+	Password string    `json:"password" validate:"required"`
+	UserID   uuid.UUID `json:"user_id" gorm:"index:userID;not null,column:user_id" validate:"required"`
+	User     User      `json:"-" validate:"-"`
+}
+
+// Validate user password fields
+func (u *UserPassword) Validate() error {
+	return validate.Struct(u)
+}
+
+// HashPassword hashes user password and returns userPassword Model
+func HashPassword(password, userID string) (*UserPassword, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := uuid.Parse(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &UserPassword{
+		Password: string(bytes),
+		UserID:   id,
+	}, nil
+}
+
+// CheckPasswordHash checks if password matches hash
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
